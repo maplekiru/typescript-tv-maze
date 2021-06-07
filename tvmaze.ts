bc65a4e5043b583d530149cfde47c74c53c92073
Kiru <kirushanth.ravindran@gmail.com>
2021-06-07 16:47:18 -0700

    update search for ts

--- tvmaze.ts
+++ tvmaze.ts
@@ -4,8 +4,16 @@ import * as $ from 'jquery';
 const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
+const TV_MAZE_API = 'http://api.tvmaze.com'
 
+const DEFAULT_IMG = `https://tinyurl.com/tv-missing`;
 
+interface Show {
+    id: number,
+    name: string,
+    summary: string,
+    image: string
+}
 /** Given a search term, search for tv shows that match that query.
  *
  *  Returns (promise) array of show objects: [show, show, ...].
@@ -13,41 +21,36 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm(term) {
-  // ADD: Remove placeholder & make request to TVMaze search shows API.
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-           normal lives, modestly setting aside the part they played in
-           producing crucial intelligence, which helped the Allies to victory
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+async function getShowsByTerm(term: string): Promise<Show[]> {
+    // ADD: Remove placeholder & make request to TVMaze search shows API.
+
+    const res = await axios.get(`${TV_MAZE_API}/search/shows?q=${term}`)
+    const shows: [] = res.data;
+
+    const showsInfo: Show[] = shows.map((show: Record<string, any>) => {
+        const  {id, name, summary} = show.show
+        const image = show.show.image && show.show.image.medium || DEFAULT_IMG ;
+
+        return { id, name, summary, image }
+    })
+
+    return showsInfo
 }
 
 
 /** Given list of shows, create markup for each and to DOM */
 
 function populateShows(shows) {
-  $showsList.empty();
+    console.log("Populate Shows: shows", shows)
+    $showsList.empty();
 
-  for (let show of shows) {
-    const $show = $(
-        `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+    for (let show of shows) {
+        const $show = $(
+            `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
-              alt="Bletchly Circle San Francisco"
+              src=${show.image}
+              alt=${show.name}"
               class="w-25 mr-3">
            <div class="media-body">
              <h5 class="text-primary">${show.name}</h5>
@@ -60,7 +63,8 @@ function populateShows(shows) {
        </div>
       `);
 
-    $showsList.append($show);  }
+        $showsList.append($show);
+    }
 }
 
 
@@ -68,17 +72,18 @@ function populateShows(shows) {
  *    Hide episodes area (that only gets shown if they ask for episodes)
  */
 
-async function searchForShowAndDisplay() {
-  const term = $("#searchForm-term").val();
-  const shows = await getShowsByTerm(term);
+async function searchForShowAndDisplay(): Promise<void> {
+    const term: string = $("#searchForm-term").val() as string;
+    const shows = await getShowsByTerm(term);
 
-  $episodesArea.hide();
-  populateShows(shows);
+    $episodesArea.hide();
+    populateShows(shows);
 }
 
 $searchForm.on("submit", async function (evt) {
-  evt.preventDefault();
-  await searchForShowAndDisplay();
+    evt.preventDefault();
+    await searchForShowAndDisplay();
+    console.log("Search handler");
 });
 
 
